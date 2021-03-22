1c16145b01573727e04639eeff4cac1541dbd179
AbdulRafay24 <AbdulRafay24@users.github.com>
2021-03-22 13:15:06 +0500

    second commit

--- app.js
+++ app.js
@@ -46,11 +46,6 @@ mongoose
     console.log(err);
   });
 
-//Development
-// app.listen(3000, () => {
-//   console.log("server is running http://localhost:3000");
-// });
-
 //Production
 var server = app.listen(process.env.PORT || 3000, function (){
   var port = server.address().port;
